76a73505db272f53a796979cf5e143096381a717
Raymond Tukpe <rtukpe@gmail.com>
2023-10-20 13:28:32 +0100

    Update Kafka functionality for error handling

--- src/Api/kafka.ts
+++ src/Api/kafka.ts
@@ -6,29 +6,29 @@ import { Partitioners, Producer } from 'kafkajs';
 
 export class Kafka {
     private client: Client;
-    private producer: Producer;
+    private producer?: Producer;
     private isConnected: boolean = false;
 
     constructor(client: Client) {
         this.client = client;
-        if (!this.client.kafka) {
-            throw new ConfigException('the kafka client is not configured');
-        }
-
-        this.producer = this.client.kafka.kafkaClient.producer({ createPartitioner: Partitioners.LegacyPartitioner });
-        this.producer.on('producer.connect', () => {
+        this.producer = this.client.kafka?.kafkaClient.producer({ createPartitioner: Partitioners.LegacyPartitioner });
+        this.producer?.on('producer.connect', () => {
             this.isConnected = true;
         });
     }
 
     async writeEvent(payload: CreateEvent | CreateFanOutEvent) {
+        if (!this.client.kafka) {
+            throw new ConfigException('the kafka client is not configured');
+        }
+
         try {
             if (!this.isConnected) {
-                await this.producer.connect();
+                await this.producer?.connect();
             }
 
             const { kafkaClient, ...opts } = this.client.kafka!;
-            return await this.producer.send({
+            return await this.producer?.send({
                 ...opts,
                 messages: [{ value: JSON.stringify(payload) }],
             });
